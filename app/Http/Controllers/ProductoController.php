65211e0ab268b778b55c1c1e559a5221ad405b75
alexander231714 <alexander.carrillos@catolica.edu.sv>
2023-12-08 18:46:25 -0600

    Nombres en vez de id

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -4,35 +4,60 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Producto;
+use App\Models\Categoria;
+use App\Models\Proveedor;
 
 class ProductoController extends Controller
 {
     public function index()
     {
-        return Producto::with(['categoria', 'proveedor'])->get();
+        $productos = Producto::with(['categoria', 'proveedor'])->get();
+        return response()->json($productos);
     }
 
     public function show($id)
     {
-        return Producto::findOrFail($id);
+        $producto = Producto::with(['categoria', 'proveedor'])->findOrFail($id);
+        return response()->json($producto);
     }
 
     public function store(Request $request)
     {
-        return Producto::create($request->all());
+        $this->validate($request, [
+            'idcategoria' => 'required|exists:categorias,idcategoria',
+            'idproveedor' => 'required|exists:proveedores,idproveedor',
+            'nombre_producto' => 'required',
+            'precio' => 'required',
+            'stock' => 'required',
+            'descripcion' => 'required',
+            'imagen' => 'required',
+        ]);
+
+        $producto = Producto::create($request->all());
+        return response()->json($producto, 201);
     }
 
     public function update(Request $request, $idproducto)
     {
+        $this->validate($request, [
+            'idcategoria' => 'required|exists:categorias,idcategoria',
+            'idproveedor' => 'required|exists:proveedores,idproveedor',
+            'nombre_producto' => 'required',
+            'precio' => 'required',
+            'stock' => 'required',
+            'descripcion' => 'required',
+            'imagen' => 'required',
+        ]);
+
         $producto = Producto::findOrFail($idproducto);
         $producto->update($request->all());
-        return $producto;
+        return response()->json($producto, 200);
     }
 
     public function destroy($id)
     {
         $producto = Producto::findOrFail($id);
         $producto->delete();
-        return 204;
+        return response()->json(null, 204);
     }
 }
